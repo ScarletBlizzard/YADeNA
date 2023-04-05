adde2df7196d8c43cad8d8268d7e4e8f00d8e6cf
Max <mesushkov@gmail.com>
2023-04-05 21:53:26 +0300

    Refactor to comply with PEP8

--- assembler.py
+++ assembler.py
@@ -13,34 +13,36 @@ def create_parser():
     # Required arguments
     required = parser.add_argument_group('required named arguments')
     required.add_argument('-r', '--read_len', required=True, type=int,
-                        help='Length of a read')
+                          help='Length of a read')
     required.add_argument('-r1', '--reads1', required=True,
-                        help='File containing reads in FASTQ format')
+                          help='File containing reads in FASTQ format')
     required.add_argument('-r2', '--reads2', required=True,
-                        help='File containing reads in FASTQ format')
+                          help='File containing reads in FASTQ format')
     required.add_argument('-c1', '--contig1', required=True,
-                        help='File containing left contig in FASTA format')
+                          help='File containing left contig in FASTA format')
     required.add_argument('-c2', '--contig2', required=True,
-                        help='File containing right contig in FASTA format')
+                          help='File containing right contig in FASTA format')
     required.add_argument('-a1', '--alignments1', required=True,
-                        help='File containing read alignments')
+                          help='File containing read alignments')
     required.add_argument('-a2', '--alignments2', required=True,
-                        help='File containing read alignments')
+                          help='File containing read alignments')
     # Optional arguments
     parser.add_argument('-d', '--read_len_divisor', type=int, default=3,
-                        help=('How many times the minimum overlap length is smaller'
-                        'than the read length (used when min_overlap_len not given)'))
+                        help=('How many times the minimum overlap length is '
+                              'smaller than the read length '
+                              '(used when min_overlap_len not given)'))
     parser.add_argument('-o', '--min_overlap_len', type=int,
                         help='Minimum length of reads overlap')
     parser.add_argument('-at', '--alignments_type', choices=['art'],
-                        help='Name of software that generated alignments files')
+                        help='Name of program that generated alignments files')
     return parser
 
 
 def overlap_len(s1, s2, min_len):
     """
     Receives strings s1 and s2.
-    Returns the length of the longest suffix of s1 that matches the prefix of s2.
+    Returns the length of the longest suffix of s1 that matches
+    the prefix of s2.
     """
     start = 0
     while True:
@@ -68,11 +70,11 @@ def create_overlap_graph(reads, contigs, min_overlap_len):
 
     c1, c2 = contigs.values()
     for r in reads.values():
-        # Find overlaps between suffix of left contig and prefixes of all reads
+        # Find overlaps between suffix of left contig and prefixes of reads
         o_len = overlap_len(c1.seq, r.seq, min_overlap_len)
         if o_len > 0:
             graph[c1.id].append((r.id, o_len))
-        # Find overlaps between prefix of right contig and suffixes of all reads
+        # Find overlaps between prefix of right contig and suffixes of reads
         o_len = overlap_len(r.seq, c2.seq, min_overlap_len)
         if o_len > 0:
             graph[r.id].append((c2.id, o_len))
@@ -122,21 +124,25 @@ def orient(reads, orientation):
             r.seq = r.seq.reverse_complement()
 
 
-def assemble(reads, contigs, orientation, read_len, read_len_divisor, min_overlap_len=None):
+def assemble(
+        reads, contigs, orientation, read_len,
+        read_len_divisor, min_overlap_len=None):
     """Returns target sequence based on args."""
     if len(contigs) != 2:
-        raise ValueError('contigs_file must contain only left and right contigs in FASTA format (left goes first)')
-    if ids := set(reads) & set(contigs): # check for duplicate ids
+        raise ValueError(('contigs_file must contain only left and right'
+                          'contigs in FASTA format (left goes first)'))
+    if ids := set(reads) & set(contigs):  # check for duplicate ids
         raise ValueError(f'Some reads and contigs have the same ids: {*ids,}')
 
     if orientation:
         orient(reads, orientation)
 
-    graph = create_overlap_graph(reads, contigs, min_overlap_len or read_len//read_len_divisor)
+    graph = create_overlap_graph(
+            reads, contigs, min_overlap_len or read_len//read_len_divisor)
 
-    (l_cont_id, l_cont), (r_cont_id, r_cont), *_ = contigs.items()
-    reads[l_cont_id], reads[r_cont_id] = l_cont, r_cont # treat contigs like reads
-    return traverse(graph, reads, l_cont, r_cont)
+    (l_con_id, l_con), (r_con_id, r_con), *_ = contigs.items()
+    reads[l_con_id], reads[r_con_id] = l_con, r_con  # treat contigs like reads
+    return traverse(graph, reads, l_con, r_con)
 
 
 def main():
@@ -151,10 +157,12 @@ def main():
 
     orientation = None
     if args.alignments_type == 'art':
-        orientation = util.parse_art_orientation((args.alignments1, args.alignments2))
+        orientation = util.parse_art_orientation(
+                (args.alignments1, args.alignments2))
 
-    return assemble(reads, contigs, orientation, args.read_len, args.read_len_divisor,
-                    min_overlap_len=args.min_overlap_len)
+    return assemble(
+            reads, contigs, orientation, args.read_len,
+            args.read_len_divisor, min_overlap_len=args.min_overlap_len)
 
 
 if __name__ == '__main__':
--- test_assembler.py
+++ test_assembler.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser
 import os
 
-from Bio import SeqIO, SeqRecord, Seq, Align
+from Bio import SeqIO, Align
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 import assembler
 import util
@@ -11,12 +13,15 @@ def create_parser():
     """Returns parser for command line arguments."""
     parser = ArgumentParser()
     parser.add_argument('max_tests_cnt', nargs='?', type=int, default=-1,
-                        help='How many times at most to run assembler with different data')
+                        help=('How many times at most to run assembler with '
+                              'different data'))
     parser.add_argument('-r', '--read_len', type=int,
-                        help='Only assemble sequences with reads of this length')
+                        help='Only assemble sequences with reads of this '
+                             'length')
     parser.add_argument('-d', '--read_len_divisor', type=int, default=3,
-                        help=('How many times the minimum overlap length is smaller'
-                        'than the read length (used when min_overlap_len not given)'))
+                        help=('How many times the minimum overlap length is '
+                              'smaller than the read length '
+                              '(used when min_overlap_len not given)'))
     parser.add_argument('-o', '--min_overlap_len', type=int,
                         help='Minimum length of reads overlap')
     parser.add_argument('--test_data_dir', default='test_data/out_dir',
@@ -37,11 +42,13 @@ def test():
     else:
         description.append(f'only of length {args.read_len}')
     if not args.min_overlap_len:
-        description.append(f'with minimum overlap length {args.read_len_divisor} times smaller than the read length')
+        description.append(' '.join(('with minimum overlap length',
+                                     str(args.read_len_divisor),
+                                     'times smaller than the read length')))
     else:
-        description.append(f'with minimum overlap length {args.min_overlap_len}')
-    print(' '.join(description))
-    print()
+        description.append('with minimum overlap length ' +
+                           str(args.min_overlap_len))
+    print(' '.join(description), end='\n\n')
 
     aligner = Align.PairwiseAligner()
     aligner.mode = 'local'
@@ -50,34 +57,46 @@ def test():
     for data_dir in os.listdir(args.test_data_dir):
         if args.max_tests_cnt >= 0 and tests_cnt >= args.max_tests_cnt:
             break
-     
-        read_len, gap_len = map(int, data_dir.split('_')[1:3]) # name of data_dir should be like "sim_{read_len}_{gap_len}"
+
+        # Expecting data_dir to be like 'sim_{read_len}_{gap_len}'
+        read_len, gap_len = map(int, data_dir.split('_')[1:3])
         if args.read_len and args.read_len != read_len:
             continue
         tests_cnt += 1
 
         if not args.summary:
-            print(f'[Read length: {read_len} | Gap length: {gap_len} | Sequence length: {2*read_len + gap_len}]')
+            print(' | '.join((f'[Read length: {read_len}',
+                              f'Gap length: {gap_len}',
+                              f'Sequence length: {2*read_len + gap_len}]')))
 
-        data_dir_path = os.path.join(os.path.dirname(__file__), args.test_data_dir, data_dir)
+        data_dir_path = os.path.join(os.path.dirname(__file__),
+                                     args.test_data_dir, data_dir)
         target_file = f'{data_dir_path}/target.fa'
         target_seq = next(SeqIO.parse(target_file, 'fasta')).seq
 
-        reads = {**SeqIO.to_dict(SeqIO.parse(f'{data_dir_path}/dat1.fq', 'fastq')),
-                 **SeqIO.to_dict(SeqIO.parse(f'{data_dir_path}/dat2.fq', 'fastq'))}
+        reads = {
+            **SeqIO.to_dict(SeqIO.parse(f'{data_dir_path}/dat1.fq', 'fastq')),
+            **SeqIO.to_dict(SeqIO.parse(f'{data_dir_path}/dat2.fq', 'fastq')),
+        }
 
-        contigs = {'left_contig': SeqRecord.SeqRecord(seq=Seq.Seq(target_seq[:read_len]),
-                                                      id='left_contig', name='left_contig',
-                                                      description='left_contig', dbxrefs=[]),
-                  'right_contig': SeqRecord.SeqRecord(seq=Seq.Seq(target_seq[len(target_seq)-read_len:]),
-                                                      id='right_contig', name='right_contig',
-                                                      description='right_contig', dbxrefs=[])}
+        contigs = {
+            'l_cont': SeqRecord(seq=Seq(target_seq[:read_len]),
+                                id='l_cont',
+                                name='l_cont',
+                                description='l_cont'),
+            'r_cont': SeqRecord(seq=Seq(target_seq[len(target_seq)-read_len:]),
+                                id='r_cont',
+                                name='r_cont',
+                                description='r_cont'),
+        }
 
-        orientation = util.parse_art_orientation([f'{data_dir_path}/dat{i}.aln' for i in (1, 2)])
+        orientation = util.parse_art_orientation(
+                [f'{data_dir_path}/dat{i}.aln' for i in (1, 2)])
 
         try:
-            result_seq = assembler.assemble(reads, contigs, orientation, read_len,
-                                            args.read_len_divisor, args.min_overlap_len)
+            result_seq = assembler.assemble(
+                    reads, contigs, orientation, read_len,
+                    args.read_len_divisor, args.min_overlap_len)
             if result_seq == target_seq:
                 correct_cnt += 1
                 if not args.summary:
@@ -85,10 +104,15 @@ def test():
             else:
                 wrong_cnt += 1
                 if not args.summary:
-                    print('Warning: resulting and target sequences differ')
-                    print(f'- Identity: {util.compute_identity(aligner, target_seq, result_seq)}')
-                    print(f'- Result: {result_seq}')
-                    print(f'- Target: {target_seq}')
+                    identity = util.compute_identity(
+                            aligner, target_seq, result_seq)
+                    print(
+                        'Warning: resulting and target sequences differ',
+                        f'- Identity: {identity}',
+                        f'- Result: {result_seq}',
+                        f'- Target: {target_seq}',
+                        sep='\n'
+                    )
         except assembler.WrongPathError:
             failed_cnt += 1
             if not args.summary:
@@ -96,12 +120,13 @@ def test():
         if not args.summary:
             print()
 
-    print('[SUMMARY]')
-    print(f'Correctly assembled {correct_cnt}/{tests_cnt} simulated sequences...')
+    summary = ['[SUMMARY]']
+    summary.append(f'Correctly assembled: {correct_cnt}/{tests_cnt}')
     if wrong_cnt > 0:
-        print(f'Incorrectly assembled {wrong_cnt}/{tests_cnt} simulated sequences...')
+        summary.append(f'Incorrectly assembled: {wrong_cnt}/{tests_cnt}')
     if failed_cnt > 0:
-        print(f'Could not assemble {failed_cnt}/{tests_cnt} simulated sequences...')
+        summary.append(f'Could not assemble: {failed_cnt}/{tests_cnt}')
+    print('\n'.join(summary))
 
 
 if __name__ == '__main__':
