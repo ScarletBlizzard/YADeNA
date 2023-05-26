d7c1edb8fc4c5bdf96ea18ba83a33a63bf5ea115
Max <mesushkov@gmail.com>
2023-05-26 20:54:01 +0300

    Show overlap length on edges of graph

--- overlap_graph.py
+++ overlap_graph.py
@@ -6,26 +6,26 @@ import graphviz
 
 class OverlapGraph:
     def __init__(self, name='', visualize=False):
-        self.__graph = defaultdict(list)
-        self.__visualize = visualize
-        if self.__visualize:
-            self.__name = name + '_graph.gv'
-            self.__graph_visualizer = graphviz.Digraph(self.__name)
+        self._graph = defaultdict(list)
+        self._visualize = visualize
+        if self._visualize:
+            self._name = name + '_graph.gv'
+            self._graph_visualizer = graphviz.Digraph(self._name)
 
     def add_child(self, parent_id: str, child_id: str, overlap_len: int):
-        heapq.heappush(self.__graph[parent_id], (-overlap_len, child_id))
-        if self.__visualize:
-            self.__graph_visualizer.node(parent_id, parent_id)
-            self.__graph_visualizer.node(child_id, child_id)
-            self.__graph_visualizer.edge(parent_id, child_id)
+        heapq.heappush(self._graph[parent_id], (-overlap_len, child_id))
+        if self._visualize:
+            self._graph_visualizer.node(parent_id, parent_id)
+            self._graph_visualizer.node(child_id, child_id)
+            self._graph_visualizer.edge(parent_id, child_id, str(overlap_len))
 
     def get_next_child(self, parent_id):
         try:
-            child = heapq.heappop(self.__graph[parent_id])
+            child = heapq.heappop(self._graph[parent_id])
             return (child[1], -child[0])
         except IndexError:
             return None
 
     def visualize(self):
-        if self.__visualize:
-            self.__graph_visualizer.render(self.__name, view=False)
+        if self._visualize:
+            self._graph_visualizer.render(self._name, view=False)
